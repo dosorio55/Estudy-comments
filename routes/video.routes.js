f8b9a18656afa36d3499f564b4b4d4506ee7493c
dosorio55 <dosorio55@gmail.com>
2022-04-24 22:16:50 +0200

    Delete, req y post listos

--- routes/video.routes.js
+++ routes/video.routes.js
@@ -4,19 +4,53 @@ import { Video } from "../models/Video.js";
 
 const videoRoutes = express.Router();
 
-videoRoutes.get('/', async(req, res) => {
-   let videos = await Video.find()
-/*     const { type } = req.query;
+videoRoutes.get('/', async (req, res) => {
 
-    let videos = [];
+    const videos = await Video.find();
 
-    if (type) {
-    } else {
-        
+    res.send(videos);
+
+/*     const { minDate, maxDate } = req.query;
+    const dateFilters = {
+        ...(minDate && {$gt: minDate}),
+        ...(maxDate && {$lt: maxDate})
+    }
+
+    try {
+        const filters = Object.keys(dateFilters).length > 0 ? { creationDate: dateFilters} : undefined;
+        const videos = await Video.find(filters)
+    } catch (error) {
+        return res.status(500).json(error)
     } */
-    res.send(videos)
 });
 
-export { videoRoutes}
+videoRoutes.post('/', async (req, res, next) =>{
+    try {
+        const newVideo = new Video ({
+            title: req.body.title,
+            link: req.body.link
+        })
+
+        const createdVideo = await newVideo.save();
+        return res.status(201).json(createdVideo)
+    } catch (error) {
+        next(error)
+    }
+});
+
+videoRoutes.delete('/:id', async (req, res, next) => {
+try {
+    const { id } = req.params;
+    
+    const deleteVideo = await Video.findByIdAndDelete(id);
+    return res.status(200).json(deleteVideo)
+    
+} catch (error) {
+    return next(error)
+}
 
 
+})
+
+
+export { videoRoutes }
